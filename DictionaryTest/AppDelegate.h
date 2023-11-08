3ae930e6824707412bf46c2288990d633fed2993
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-11-08 15:47:06 -0500

    Initial Commit

---
+++ DictionaryTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  DictionaryTest
+//
+//  Created by Sergey Lukaschuk on 2023-11-08.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DictionaryTest/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  DictionaryTest
+//
+//  Created by Sergey Lukaschuk on 2023-11-08.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
